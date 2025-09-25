d4618fa790e20dc0876a32b5f6cbfaf2a0b99293
ObedLandu <obedlandu08@gmail.com>
2025-09-25 16:30:12 +0100

    init believemy

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Logo from "./components/Logo/Logo";
+import Pokecard from "./components/Pokecard/Pokecard";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    // State
+    const [pokemons, setPokemons] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <Logo />
+
+            <div>
+                {/* Pokemons */}
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 max-w-7xl mx-auto mt-10 md:p-0 p-5">
+                    {pokemons.map((pokemon, index) => (
+                        <Pokecard key={index} pokemon={pokemon} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default App
