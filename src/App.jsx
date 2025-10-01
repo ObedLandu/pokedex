8c110c28019b9d3b91920306ad055412c9fa8b13
ObedLandu <obedlandu08@gmail.com>
2025-10-01 14:03:48 +0100

    CreateBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,13 @@
-import { useState, useEffect } from "react";
-import Logo from "./components/Logo/Logo";
-import Pokecard from "./components/Pokecard/Pokecard";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
-  // State
-  const [pokemons, setPokemons] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
 
-  // récupérer les 30 premiers pokemons
-  const fetchPokemons = async (add = false) => {
-    setLoading(true);
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=30${
-        add && "&offset=" + pokemons.length
-      }`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    //console.log(response);
-    const data = await response.json();
-    //console.log(data);
-
-    //details des pokemons
-    const promises = data.results.map(async (pokemon) => {
-      const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      //console.log(response);
-      return await response.json();
-    });
-    const results = await Promise.all(promises);
-    //console.log(results);
-    setPokemons([...pokemons, ...results]);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchPokemons();
-  }, []);
-
-  return (
-    <div>
-      <Logo />
-      <div>
-        {/* Pokemons */}
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 max-w-7xl mx-auto mt-10 md:p-0 p-5">
-          {pokemons.map((pokemon, index) => (
-            <Pokecard key={index} pokemon={pokemon} />
-          ))}
-        </div>
-        {/* loader */}
-        {loading && (
-          <div className="text-center my-10">
-            <div className="flex justify-center text-center text-white">chargement</div>
-          </div>
-        )}
-
-        {/* get more pokemons */}
-        <div className="text-center my-10">
-          <button
-            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300"
-            onClick={() => fetchPokemons(true)}
-          >
-            Get more pokemons
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,82 @@
 import { useState, useEffect } from "react";
 import Pokecard from "../components/Pokecard/Pokecard";
+import Logo from "../components/Logo/Logo";
+import { toast } from "react-toastify";
 
 export default function Home() {
-    // States
-    const [pokemons, setPokemons] = useState([]);
-    const [loading, setLoading] = useState(false);
+  // States
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    return (
-        <div>
-            {/* Pokemons */}
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 max-w-7xl mx-auto mt-10 md:p-0 p-5">
-                {pokemons.map((pokemon, index) => (
-                    <Pokecard key={index} pokemon={pokemon} />
-                ))}
-            </div>
-
-            {/* Loading */}
-            {loading && (
-                <div className="flex justify-center items-center text-white mt-5">
-                    Chargement...
-                </div>
-            )}
-
-            {/* Add */}
-            <div className="flex justify-center my-10">
-                <button
-                    className="bg-white hover:bg-gray-50 rounded-full text-black py-2 px-5 text-lg font-semibold shadow-lg hover:shadow-xl transition duration-150 disabled:opacity-80 disabled:cursor-wait"
-                    disabled={loading}
-                >
-                    Encore plus de Pokémons !
-                </button>
-            </div>
-        </div>
+  // récupérer les 30 premiers pokemons
+  const fetchPokemons = async (add = false) => {
+    setLoading(true);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=30${
+        add && "&offset=" + pokemons.length
+      }`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     );
+    //console.log(response);
+    const data = await response.json();
+    //console.log(data);
+
+    //details des pokemons
+    const promises = data.results.map(async (pokemon) => {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      //console.log(response);
+      return await response.json();
+    });
+    const results = await Promise.all(promises);
+    //console.log(results);
+    setPokemons([...pokemons, ...results]);
+    setLoading(false);
+    toast.success("Pokemons chargés avec succès !");
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div>
+      <Logo />
+      {/* Pokemons */}
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 max-w-7xl mx-auto mt-10 md:p-0 p-5">
+        {pokemons.map((pokemon, index) => (
+          <Pokecard key={index} pokemon={pokemon} />
+        ))}
+      </div>
+
+      {/* Loading */}
+      {loading && (
+        <div className="flex justify-center items-center text-white mt-5">
+          Chargement...
+        </div>
+      )}
+
+      {/* Add */}
+      <div className="text-center my-10">
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300"
+          onClick={() => fetchPokemons(true)}
+        >
+          Get more pokemons
+        </button>
+      </div>
+    </div>
+  );
 }
