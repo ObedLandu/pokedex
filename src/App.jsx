65f7a7dac3a91197aae1d1b1b263ee7bb964a0d0
ObedLandu <obedlandu08@gmail.com>
2025-10-01 12:32:24 +0100

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import Pokecard from "./components/Pokecard/Pokecard";
 export default function App() {
   // State
   const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // récupérer les 30 premiers pokemons
-  const fetchPokemons = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  const fetchPokemons = async (add = false) => {
+    setLoading(true);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=30${
+        add && "&offset=" + pokemons.length
+      }`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     //console.log(response);
     const data = await response.json();
     //console.log(data);
@@ -29,18 +36,18 @@ export default function App() {
           },
         }
       );
-      console.log(response);
+      //console.log(response);
       return await response.json();
     });
     const results = await Promise.all(promises);
-    console.log(results);
-    setPokemons(results);
+    //console.log(results);
+    setPokemons([...pokemons, ...results]);
+    setLoading(false);
   };
 
-  useEffect (() => {
+  useEffect(() => {
     fetchPokemons();
-  })
-  
+  }, []);
 
   return (
     <div>
@@ -51,6 +58,22 @@ export default function App() {
           {pokemons.map((pokemon, index) => (
             <Pokecard key={index} pokemon={pokemon} />
           ))}
+        </div>
+        {/* loader */}
+        {loading && (
+          <div className="text-center my-10">
+            <div className="flex justify-center text-center text-white">chargement</div>
+          </div>
+        )}
+
+        {/* get more pokemons */}
+        <div className="text-center my-10">
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300"
+            onClick={() => fetchPokemons(true)}
+          >
+            Get more pokemons
+          </button>
         </div>
       </div>
     </div>
