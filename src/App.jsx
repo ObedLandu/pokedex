7a967f4359d151c9caf795a76cb3cc358a7fd4c4
ObedLandu <obedlandu08@gmail.com>
2025-10-02 16:42:50 +0100

    creation des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
     },
   ]);
 
